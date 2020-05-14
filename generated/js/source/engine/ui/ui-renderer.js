067c44a986cf0143885e74b2bdc38291e759172f
jacklehamster <jacklehamster@users.noreply.github.com>
2020-05-14 20:24:28 +0000

    deploy: 89c8c006268afa6be0ab47b6d79bade991505d17

--- generated/js/source/engine/ui/ui-renderer.js
+++ generated/js/source/engine/ui/ui-renderer.js
@@ -14,93 +14,9 @@
 
 class UIRenderer {
 	constructor(sceneUI) {
-//	constructor(canvas, canvasRenderer, workerManager, sceneUI) {
 		this.sceneUI = sceneUI;
 		this.elements = {};
-		// this.canvas = canvas;
-		// this.canvasRenderer = canvasRenderer;
-		// this.workerManager = workerManager;
-		// this.root = null;
-		// this.elements = {};
-//		window.addEventListener("resize", e => this.onResize());
 	}
-
-	// ensureRoot() {
-	// 	if (this.root === null) {
-	// 		const { canvas } = this;
-	// 		this.root = canvas.parentElement.appendChild(document.createElement("div"));
-	// 		this.root.style.position = "absolute";
-	// 		this.root.classList.add("unselectable");
-	// 		this.root.style.top = `${canvas.offsetTop}px`;
-	// 		this.root.style.left = `${canvas.offsetLeft}px`;
-	// 		this.root.style.display = "flex";
-	// 	}
-	// 	return this.root;
-	// }
-
-	// onResize() {
-	// 	if (this.root) {
-	// 		this.root.style.top = `${this.canvas.offsetTop}px`;
-	// 		this.root.style.left = `${this.canvas.offsetLeft}px`;
-	// 	}
-	// }
-
-	// setupEvents(dom, onClick, instanceIndex) {
-	// 	dom.addEventListener("click", () => onClick.run(instanceIndex));
-	// }
-
-	// createElement(elementId, instanceIndex, type, hasOnClick) {
-	// 	const { elements, workerManager } = this;
-	// 	let dom = elements[elementId] ? elements[elementId].dom : null;
-	// 	if (!dom) {
-	// 		dom = document.createElement(type);
-	// 		dom.id = elementId;
-	// 		elements[elementId] = { dom, type, updated: 0, canvas: null, lastDraw: 0 };
-	// 		if (workerManager && hasOnClick) {
-	// 			dom.addEventListener("click", () => workerManager.onClickUI(dom.id, instanceIndex));
-	// 		}
-	// 	}
-	// }
-
-	// setParent(elementId, parent) {
-	// 	const { elements } = this;
-	// 	elements[elementId].parent = parent;
-	// 	elements[elementId].needReparent = true;		
-	// }
-
-	// setClass(elementId, classList) {
-	// 	const { elements } = this;
-	// 	const { dom } = elements[elementId];
-	// 	dom.classList.add(...classList.split(" "));
-	// }
-
-	// setStyle(elementId, s, value) {
-	// 	const { elements } = this;
-	// 	const { dom } = elements[elementId];
-	// 	dom.style[s] = value;		
-	// }
-
-	// setText(elementId, text) {
-	// 	const { elements } = this;
-	// 	const { dom } = elements[elementId];
-	// 	dom.innerText = text;		
-	// }
-
-	// setSize(elementId, width, height) {
-	// 	const { elements } = this;
-	// 	const { dom } = elements[elementId];
-	// 	if (width) {
-	// 		dom.width = width;
-	// 	}
-	// 	if (height) {
-	// 		dom.height = height;
-	// 	}		
-	// }
-
-	// setCanvas(elementId, canvas) {
-	// 	const { elements } = this;
-	// 	elements[elementId].canvas = canvas;
-	// }
 
 	triggegClick(id, instanceIndex) {
 		const { elements } = this;
@@ -113,7 +29,7 @@ class UIRenderer {
 		const { elements, sceneUI } = this;
 
 		for (let i = 0; i < uiSprites.length; i++) {
-			const { type, id, classList, style, parent, innerText, instanceIndex, width, height, hidden, updateTimes, events: { onClick }, canvas } = uiSprites[i];
+			const { type, id, classList, style, parent, innerText, instanceIndex, width, height, hidden, updateTimes, events, canvas } = uiSprites[i];
 			if (id) {
 				if (!elements[id]) {
 					elements[id] = {
@@ -123,23 +39,17 @@ class UIRenderer {
 				const indexSuffix = !instanceIndex ? "" : `-${instanceIndex}`;
 				const elementId = `${id}${indexSuffix}`;
 				if (updateTimes.id === now) {
-					const hasOnClick = onClick && onClick.dynamic;
+					const hasOnClick = events && events.onClick && events.onClick.dynamic;
 					sceneUI.createElement(elementId, instanceIndex, type, hasOnClick);
 					if (hasOnClick) {
-						elements[id].onClick = onClick;
+						elements[id].onClick = events.onClick;
 					}
 				}
-				// if (!elements[elementId]) {
-					// if (hasOnClick) {
-					// 	this.setupEvents(elements[elementId].dom, onClick, instanceIndex);
-					// }
-				// }
 
 				if (updateTimes.parent === now) {
 					sceneUI.setParent(elementId, parent);
 				}
 
-//				const dom = elements[elementId].dom;
 				if (updateTimes.classList === now) {
 					sceneUI.setClass(elementId, classList);
 				}
@@ -172,13 +82,8 @@ class UIRenderer {
 
 		for (let id in elements) {
 			if (elements[id].updated !== now) {
-
 				delete elements[id];
 				sceneUI.removeElement(id);
-				// if (dom.parentElement) {
-				// 	dom.parentElement.removeChild(dom);
-				// 	delete elements[e];
-				// }
 			}
 		}
 	}
@@ -190,92 +95,4 @@ class UIRenderer {
 			delete elements[id];
 		}
 	}
-
-	// shouldHide(now, phaseTimes) {
-	// 	if (phaseTimes) {
-	// 		const timeInPeriod = now % phaseTimes[phaseTimes.length - 1];
-	// 		for (let i = 0; i < phaseTimes.length; i++) {
-	// 			if (timeInPeriod < phaseTimes[i]) {
-	// 				return i % 2 === 1;	//	show-hide-show-hide
-	// 			}
-	// 		}
-	// 	}
-	// 	return false;
-	// }
-
-	// updateUI(now) {
-	// 	const { elements } = this;
-	// 	for (let e in elements) {
-	// 		const { updated, dom } = elements[e];
-	// 		if (updated !== now) {
-	// 			if (dom.parentElement) {
-	// 				dom.parentElement.removeChild(dom);
-	// 				delete elements[e];
-	// 			}
-	// 		}
-	// 	}
-
-	// 	for (let e in elements) {
-	// 		const { parent, needReparent, dom, canvas, lastDraw } = elements[e];
-	// 		if (needReparent) {
-	// 			const p = !parent ? this.ensureRoot() : elements[parent] ? elements[parent].dom : null;
-	// 			if (p) {
-	// 				if (dom.parentElement && dom.parentElement !== p) {
-	// 					dom.parentElement.removeChild(dom);
-	// 				}
-	// 				p.appendChild(dom);
-	// 			}
-	// 			delete elements[e].needReparent;
-	// 		}
-
-	// 		if (canvas && canvas.draw.length) {
-	// 			const refreshRate = canvas.refreshRate;
-	// 			if (now - (lastDraw||0) > refreshRate) {
-	// 				elements[e].lastDraw = now;	
-
-	// 				const context = dom.getContext("2d");
-	// 				for (let i = 0; i < canvas.draw.length; i++) {
-	// 					const drawList = canvas.draw[i];
-	// 					for (let d = 0; d < drawList.length; d++) {
-	// 						const { type, src, hidden, x, y, width, height, frame, strokeStyle, lineWidth, phaseTimes } = drawList[d];
-	// 						const shouldHide = hidden || this.shouldHide(now, phaseTimes);
-	// 						if (!shouldHide) {
-	// 							switch(type) {
-	// 								case "clear":
-	// 									context.clearRect(0, 0, dom.width, dom.height);
-	// 								break;
-	// 								case "image":
-	// 									if (src) {
-	// 										this.canvasRenderer.drawToCanvas(src, x, y, width, height, dom, frame);
-	// 									}
-	// 								break;
-	// 								case "rect":
-	// 									context.strokeStyle = strokeStyle;
-	// 									context.lineWidth = lineWidth;
-	// 									context.beginPath();
-	// 									context.rect(x, y, width, height);
-	// 									context.stroke();
-	// 								break;
-	// 							}
-	// 						}
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-	// destroy() {
-	// 	for (let i in this.elements) {
-	// 		delete this.elements[i];
-	// 	}
-	// 	if (this.root) {
-	// 		this.root.parentElement.removeChild(this.root);
-	// 		this.root = null;
-	// 	}
-	// }
 }
-
-
-
-
