20bca7cb0a50e984709af8619e70a559659b46c3
Jack Le Hamster <jacklehamster@gmail.com>
2020-06-06 14:14:54 -0700

    Add FPS

---
+++ src/engine/utils/fps.js
@@ -0,0 +1,31 @@
+/**
+	Dok-gamelib engine
+
+	Description: Game engine for producing web games easily using JavaScript and WebGL
+	Author: jacklehamster
+	Sourcode: https://github.com/jacklehamster/dok-gamelib
+	Year: 2020
+ */
+
+class FPSTracker {
+	constructor() {
+		this.accumulator = new Array(100).fill(0);
+		this.index = 0;
+	}
+
+	tick() {
+		const time = Date.now();
+		this.accumulator[this.index] = time;
+		this.index = ((this.index+1) % this.accumulator.length);
+		const timeDiff = time - this.accumulator[this.index];
+		return 1000 / (timeDiff / this.accumulator.length);
+	}
+
+	get frameRate() {
+		const { accumulator, index } = this;
+		const timeDiff =  accumulator[(index-1 + accumulator.length) % accumulator.length] - accumulator[index];
+		return 1000 / (timeDiff / accumulator.length);
+	}
+}
+
+FPSTracker.fps = new FPSTracker();
