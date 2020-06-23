5894a21f0b2a4b4f8a99fcd2430c37c6172bbe0b
jacklehamster <jacklehamster@users.noreply.github.com>
2020-06-23 00:56:00 +0000

    deploy: 628a9a4dfb95367b781c3f39bb7280dff4d9e58e

--- generated/js/source/game/scenes/slope/start.js
+++ generated/js/source/game/scenes/slope/start.js
@@ -1,7 +1,7 @@
 SceneManager.add({
 	view: {
 		cameraDistance: 15,
-		lean: ({game}) => -game.ship.dx / 10,
+		lean: ({game}) => -game.ships[0].dx / 10,
 		pos: [
 			({game}) => game.cam.x,
 			3,
@@ -19,17 +19,27 @@ SceneManager.add({
 			padding: 1,
 			frameRate: 10,
 			animations: [
+				[ "still", "0" ],
 				[ "burn", "0-2" ],
 			],
+		},
+		{
+			src: "smorf-tree",
+			spriteSize: [288, 468],
+			padding: 1,			
 		},
 	],
 	keyboard: {
 		onActionPress: ({game}) => {
+			if (game.ships[0].paused) {
+				game.ships[0].paused = false;
+			}
 			for (let i = 0; i < 20; i++) {
 				game.missiles.push({
-					x: game.ship.x,
-					z: game.ship.z - 1,
-					time: game.now - i * 10,
+					x: game.ships[0].x + game.ships[0].dx * 3,
+					z: game.ships[0].z - 1,
+					dx: game.ships[0].dx / 3,
+					time: game.now - i * 5,
 				});
 			}
 		},
@@ -39,22 +49,15 @@ SceneManager.add({
 			x: 0,
 			z: 0,
 		};
-		game.ship = {
-			x: 0,
-			z: 0,
-			dx: 0,
-			dz: 0,
-		};
-		game.missiles = [];
 	},
 	sprites: [
 		{
 			init: ({game, definition}) => {
-				definition.cols = 50;
-				definition.rows = 50;
+				definition.cols = 60;
+				definition.rows = 70;
 				definition.tiles = [];
-				for (let y = 0; y < definition.cols; y++) {
-					for (let x = 0; x < definition.rows; x++) {
+				for (let y = 0; y < definition.rows; y++) {
+					for (let x = 0; x < definition.cols; x++) {
 						definition.tiles.push({
 							x: x - definition.cols / 2, y: y - definition.rows / 2,
 							corners: [
@@ -81,16 +84,6 @@ SceneManager.add({
 						tile.x += definition.cols;
 					}
 				}
-				const { keys: { actions: { mov } }, ship, cam } = game;
-				ship.dx += mov.x * .05;
-				ship.dz += mov.y * .05;
-				ship.dx *= .9;
-				ship.dz *= .9;
-				ship.x += ship.dx;
-				ship.z += -1.5 + ship.dz * 2;
-
-				cam.x += (ship.x - cam.x) / 5;
-				cam.z += ((ship.z - 8) - cam.z) / 5;
 			},
 			src: "artic",
 			type: SpriteType.Floor,
@@ -108,23 +101,98 @@ SceneManager.add({
 			count: ({definition}) => definition.tiles.length,
 		},
 		{
-			src: "ship",
+			src: "smorf-tree",
+			type: SpriteType.Front,
+			init: ({game, definition}) => {
+				definition.trees = [];
+				for (let i = 0; i < 30; i++) {
+					const scale = Math.random() * 10;
+					definition.trees.push({
+						x:(Math.random() - .5) * 100,
+						y: scale/2,
+						z:- Math.random() * 100,
+						scale,
+					});
+				}
+			},
+			refresh: ({game, definition}) => {
+				const xPos = game.view.pos[0].get();
+				const zPos = game.view.pos[2].get();
+				for (let i = 0; i < definition.trees.length; i++) {
+					const tree = definition.trees[i];
+					if (tree.z - zPos > 10) {
+						tree.z -= 70;
+						tree.x = xPos + (Math.random()-.5) * 60;
+						tree.scale = Math.random() * 10;
+						tree.y = tree.scale/2;
+					}
+					if (tree.x - xPos > 30) {
+						tree.x -= 60;
+					} else if (tree.x - xPos < -30) {
+						tree.x += 60;
+					}
+				}
+			},
 			pos: [
-				({game}) => game.ship.x,
+				({definition: {trees}}, index) => trees[index].x,
+				({definition: {trees}}, index) => trees[index].y,
+				({definition: {trees}}, index) => trees[index].z,
+			],
+			scale: [
+				({definition: {trees}}, index) => trees[index].scale,
+				({definition: {trees}}, index) => trees[index].scale * 1.5,
+			],
+			count: ({definition: {trees}}) => trees.length,
+		},
+		{
+			src: "ship",
+			init: ({game}) => {
+				game.ships = [{
+					x: 0,
+					z: 0,
+					dx: 0,
+					dz: 0,
+					paused: true,
+				}];
+			},
+			pos: [
+				({game: { ships }}, index) => ships[index].x + ships[index].dx * 2,
 				1.5,
-				({game}) => game.ship.z,
+				({game: { ships }}, index) => ships[index].z + ships[index].dz * 2,
 			],
 			scale: [ 1.5, 1 ],
 			rotation: {
 				angle: [
 					0,
 					0,
-					({game}) => -game.ship.dx / 2,
+					({game}, index) => -game.ships[index].dx / 2,
 				],
 			},
+			refresh: ({game}, index) => {
+				const { keys: { actions: { mov } }, ships, cam } = game;
+				ships[0].dx += mov.x * .05;
+				ships[0].dz += mov.y * .05;
+
+				for (let i = 0; i < ships.length; i++) {
+					const ship = ships[i];
+					ship.dx *= .9;
+					ship.dz *= .9;
+					if (!ship.paused) {
+						ship.x += ship.dx;
+						ship.z += -1.2 + ship.dz * 2;
+					}
+				}
+				cam.x += (ships[0].x - cam.x) / 5;
+				cam.z += ((ships[0].z - 8) - cam.z) / 5;
+			},
+			animation: ({game}, index) => game.ships[index].paused ? "still" : "burn",
+			count: ({game}, index) => game.ships.length,
 		},
 		{
 			src: "artic",
+			init: ({game}) => {
+				game.missiles = [];
+			},
 			circleRadius: 1,
 			pos: [
 				({game}, index) => game.missiles[index].x,
@@ -134,8 +202,8 @@ SceneManager.add({
 			motion: {
 				time: ({game}, index) => game.missiles[index].time,
 				mov: [
-					0,
-					0,
+					({game}, index) => game.missiles[index].dx,
+					.02,
 					-.5,
 				],
 				gravity: [
