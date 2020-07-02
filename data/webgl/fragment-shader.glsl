c24e0801fa540b1c3f8035a91d688c30c7a9dbfb
Jack Le Hamster <jacklehamster@gmail.com>
2020-07-02 01:46:37 -0700

    Fix glitchy graphics.

--- data/webgl/fragment-shader.glsl
+++ data/webgl/fragment-shader.glsl
@@ -33,7 +33,7 @@ uniform vec3 uCamPosition;
 uniform vec4 uDepthEffect;
 
 vec4 getTextureColor(sampler2D textures[NUM_TEXTURES], float textureSlot, vec2 vTexturePoint) {
-	int textureInt = int(textureSlot);
+	int textureInt = int(textureSlot + .5);
 	for (int i = 0; i < NUM_TEXTURES; ++i) {
 		if (textureInt == i) {
 			return texture2D(textures[i], vTexturePoint);
